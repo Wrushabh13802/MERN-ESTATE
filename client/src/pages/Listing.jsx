55fe136b89672e258ed1e979791e2aeba4b5ea0a
Wrushabh Parkhi <wrushabhparkhi123@gmail.com>
2024-02-02 12:12:48 +0530

    Add contact landload to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -14,6 +14,7 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import Contact from '../Components/Contact';
 // import Contact from '../components/Contact';
 
 // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
@@ -141,7 +142,7 @@ export default function Listing() {
                 onClick={() => setContact(true)}
                 className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
               >
-                Contact landlord
+                Contact landOwner
               </button>
             )}
             {contact && <Contact listing={listing} />}
